a7dc2737b19fb17763916f3e81d260b14451d116
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2021-02-24 23:39:52 -0300

    test: ensure ValidationBuilder returns a list of validations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { DesktopLogin } from '@src/presentation/pages/login/'
+import { AxiosHttpClient } from '@src/infra/http/axios-http-client/axios-http-client'
+import { RemoteAuthentication } from '@src/data/usecases/authentication/remote-authentication'
+import { ValidationComposite } from '@src/validation/validators/validation-composite/validation-composite'
+import { ValidationBuilder } from '@src/validation/builder/validation-builder'
 
 const LoginPage: React.FC = () => {
+  const url = 'http://fordeves.herokuapp.com/api/login'
+  const axiosHttpClient = new AxiosHttpClient()
+  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
+  const validationComposite = ValidationComposite.build({
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build()
+  })
   return (
-    <DesktopLogin />
+    <DesktopLogin
+      authentication={remoteAuthentication}
+      validation={validationComposite}
+    />
   )
 }
 
