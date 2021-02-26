2b79455af354a5dc9e9f6c8652f405e848eeee9e
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2021-02-26 02:42:56 -0300

    fix: remove password validation temporary

--- src/presentation/pages/login/desktop/login.tsx
+++ src/presentation/pages/login/desktop/login.tsx
@@ -57,7 +57,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     try {
-      if (state.isSubmitting || state.emailError || state.passwordError) {
+      if (state.isSubmitting || state.emailError || state.passwordError || state.password === '') {
         setState({
           ...state,
           passwordError: validation.validate('password', state.password),
